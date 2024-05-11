b87746ffc949bb7c38e5779f4251b37684ab9b01
Susmita Ghosh <143573415+Susmita-Rani-Ghosh@users.noreply.github.com>
2024-05-11 22:55:46 +0000

    Added Scoring system to Rock, Paper, Scissors game (day 4)
    Used while loop and break statements to repitatively execute a block of code

--- Day 4 - Rock Paper Scissors.py
+++ Day 4 - Rock Paper Scissors.py
@@ -28,28 +28,45 @@ scissors = '''
 #Write your code below this line 👇
 import random
 
+user_points = 0
+computer_points = 0
+
 game_images = [rock, paper, scissors]
-print("Welcome to the Rock, Paper, Scissors game!\nLet's start the game")
+print("Welcome to the Rock, Paper, Scissors game! Let's start playing!")
 
-userChoice = int(
-    input(
-        "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. Type 'exit' to Exit \n"
-    ))
-if userChoice < 3 and userChoice >= 0:
-    print(game_images[userChoice])
-    print("Computer chose:")
-    computerChoice = random.randint(0, 2)
-    print(game_images[computerChoice])
+while True:
+    print(
+        f"Score:\nYour points: {user_points}\tComputer points: {computer_points}\n"
+    )
+    userChoice = int(
+        input(
+            "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. Type 3 to EXIT \n"
+        ))
+    if userChoice == 3:
+        if user_points == computer_points == 0:
+            (print("Bye! See you later!"))
+        print("Thanks for playing!")
+        break
+    if userChoice < 3 and userChoice >= 0:
+        print(game_images[userChoice])
+        print("Computer chose:")
+        computerChoice = random.randint(0, 2)
+        print(game_images[computerChoice])
 
-    if userChoice == computerChoice:
-        print("It's a draw")
-    elif userChoice == 0 and computerChoice == 2:
-        print("You win")
-    elif userChoice == 2 and computerChoice == 0:
-        print("You lose")
-    elif userChoice > computerChoice:
-        print("You win")
-    elif userChoice < computerChoice:
-        print("You Lose")
-else:
-    print("You made an invalid choice. Gmae Over")
+        if userChoice == computerChoice:
+            print("It's a draw")
+        elif userChoice == 0 and computerChoice == 2:
+            print("You win")
+            user_points += 1
+        elif userChoice == 2 and computerChoice == 0:
+            print("You lose")
+            computer_points += 1
+        elif userChoice > computerChoice:
+            print("You win")
+            user_points += 1
+        elif userChoice < computerChoice:
+            print("You Lose")
+            computer_points += 1
+        print("\n")
+    else:
+        print("***You made an invalid choice. Try again.***")
